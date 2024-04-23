6acf171e3a590fbfca1b3382a0645613fc1b1732
Jetistrim <paulogabriellima@hotmail.com>
2024-04-23 12:13:48 -0300

    updates

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,8 +1,69 @@
+import { NavLink } from "react-router-dom";
+import { styled } from "styled-components";
+
+const HeaderContainer = styled.header`
+    background-color: red;
+    display: flex;
+    justify-content: space-between;
+    padding: 7px 60px;
+    align-items: center;
+
+    & nav{
+        display: flex;
+        gap: 26px;
+        & h1{
+            color: white;
+        }
+        & ul{
+            display: flex;
+            gap: 16px;
+            & a{
+                color: white;
+                display: inline-block;
+                line-height: 46px;
+                padding: 0 16px;
+                position: relative;
+                &:hover, &.active{
+                    color: #F4796B;
+                }
+                &::after{
+                    content: "";
+                    width: 0;
+                    height: 4px;
+                    background-color: #F4796B;
+                    border-radius: 2px;
+                    position: absolute;
+                    bottom: 0;
+                    left: 50%;
+                    transition-duration: 200ms ;
+                }
+                &.active::after{
+                    width: 100%;
+                    left: 0;
+                }
+            }
+        }
+    }
+`;
+
 const Header = () => {
     return (
-        <>
-            <h1>Header</h1>
-        </>
+        <HeaderContainer>
+            <nav>
+            <h1>LOGO</h1>
+                <ul>
+                    <li>
+                        <NavLink to={'/'}>Inicio</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to={'/cardapio'}>Cardapio</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to={'/promocoes'}>Promoções</NavLink>
+                    </li>
+                </ul>
+            </nav>
+        </HeaderContainer>
     );
 }
  
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import PageLayout from "../layouts/PageLayout";
 import Home from "../pages/sitePages/Home";
+import Login from "../pages/adminPages/Login";
 
 const Path = () => {
   return (
@@ -9,6 +10,7 @@ const Path = () => {
         <Routes>
           <Route path="/" element={<PageLayout />}>
             <Route index element={<Home />}/>
+            <Route path="/cardapio" element={<Login />}/>
           </Route>
         </Routes>
       </BrowserRouter>
