c0df842a5ae11ccbc623a6c758cf6879899607f6
Jetistrim <paulogabriellima@hotmail.com>
2024-05-02 12:00:24 -0300

    update

--- src/pages/sitePages/Home/index.jsx
+++ src/pages/sitePages/Home/index.jsx
@@ -7,6 +7,13 @@ const HomeContainer = styled.section`
         font-size: 36px;
         margin-bottom: 16px;
     }
+    & input{
+        width: 400px;
+        height: 60px;
+        padding-left: 26px;
+        border-radius: 40px;
+        border: 2px solid #6B0504;
+    }
     & .cards {
         display: flex;
         gap: 20px;
@@ -47,7 +54,9 @@ const HomeContainer = styled.section`
 
 const Home = () => {
     
-    const [pizzas,setPizzas] = useState([])
+    const [pizzas,setPizzas] = useState([]);
+    const [pizzasFiltradas, setPizzasFiltradas] = useState([]);
+    const [pesquisa, setPesquisa] = useState("");
 
     async function buscarSabores(){
         const request = await fetch('http://localhost:3000/sabores');
@@ -58,14 +67,28 @@ const Home = () => {
     useEffect(() => {
         buscarSabores()
     }, [])
-    
+
+    useEffect(() => {
+        if (pesquisa){
+            setPizzasFiltradas([...pizzas.filter(pizza => pizza.nome.toLowerCase().includes(pesquisa.toLowerCase()))])
+            return;
+        }
+        setPizzasFiltradas(pizzas)
+    }, [pesquisa, pizzas])
     
     return(
         <HomeContainer>
+            <div>
+                <input 
+                type="text"
+                placeholder="Procure um sabor..."
+                onChange={(e) => setPesquisa(e.target.value)}
+                />
+            </div>
             <h1>Pizzas em destaque</h1>
             <ul className="cards">
             {
-                pizzas && pizzas.filter(p => p.promocao).map(p => (
+                pizzas && pizzasFiltradas.filter(p => p.promocao).map((p, index) => (
                     <li>
                         <img src="" alt="" />
                         <h5>{p.nome}</h5>
@@ -83,11 +106,11 @@ const Home = () => {
             <h1>Todas as Pizzas</h1>
             <ul className="cards">
             {
-                pizzas && pizzas.map(p => (
+                pizzas && pizzas.map((p, index) => (
                     <li>
                         <img src="" alt="" />
                         <h5>{p.nome}</h5>
-                        <p>p.descricao</p>
+                        <p>{p.descricao}</p>
                         <div className="preco">
                             <h6>{p.preco} </h6>
                             <button>adicionar</button>
